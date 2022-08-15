064ce5d4735a49452a459853c616528a5d17a608
Compileinfy Support <support@compileinfy.com>
2022-08-16 00:18:05 +0530

    Added launch activity

--- src/android/WakeupReceiver.java
+++ src/android/WakeupReceiver.java
@@ -3,23 +3,24 @@ package org.cordova.alarmclockplugin;
 import android.annotation.SuppressLint;
 import android.app.ActivityManager;
 import android.app.AlarmManager;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.PowerManager;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
 
-
+import com.santen.actpack.MainActivity;
+import com.santen.actpack.R;
 
 import org.apache.cordova.PluginResult;
 import org.json.JSONException;
@@ -30,13 +31,14 @@ import java.util.Date;
 import java.util.List;
 
 import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
-import static android.app.PendingIntent.getActivity;
 import static android.app.PendingIntent.getBroadcast;
+import static android.provider.Telephony.Mms.Part.TEXT;
 
 public class WakeupReceiver extends BroadcastReceiver {
 
 	private static final String LOG_TAG = "WakeupReceiver";
 	private PowerManager.WakeLock mWakeLock;
+	private MediaPlayer mMediaPlayer;
 
 	public boolean isRunning(Context ctx) {
 		ActivityManager activityManager = (ActivityManager) ctx.getSystemService(Context.ACTIVITY_SERVICE);
@@ -170,7 +172,9 @@ public class WakeupReceiver extends BroadcastReceiver {
 				}
 			}
 
-			context.startActivity(i);
+			if (WakeupPlugin.connectionCallbackContext != null || !nativeNotification) {
+				context.startActivity(i);
+			}
 
 		} catch (JSONException e){
 			e.printStackTrace();
@@ -178,6 +182,7 @@ public class WakeupReceiver extends BroadcastReceiver {
 			e.printStackTrace();
 		}
 	}
+
 
 	private void checkAndAcquireWakeLock(Context context) {
 		PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
@@ -190,17 +195,16 @@ public class WakeupReceiver extends BroadcastReceiver {
 					PowerManager.ON_AFTER_RELEASE, "Wake:Log");
 			mWakeLock.acquire(5*60*1000L /*5 minutes*/);
 
-			new Handler().postDelayed(() -> {
-				if (null != mWakeLock && mWakeLock.isHeld()) {
-					mWakeLock.release();
-				}
-			},5000);
+//			new Handler().postDelayed(() -> {
+//				if (null != mWakeLock && mWakeLock.isHeld()) {
+//					mWakeLock.release();
+//				}
+//			},5000);
 		}
 	}
 
 	private void raisePriorityNotification(Context context, Intent intent, Bundle extrasBundle) {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-			PendingIntent startAlarmPendingIntent = getActivity(context, 0, intent, FLAG_UPDATE_CURRENT);
 			String title = extrasBundle.getString("title", "Medicine");
 			String message = extrasBundle.getString("message", "Medicine");
 			String channelId = extrasBundle.getString("notificationChannelId", "wc_alarms");
@@ -209,49 +213,56 @@ public class WakeupReceiver extends BroadcastReceiver {
 			NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 			createNotificationChannel(notificationManager, channelId, channelName);
 
-			Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-			if (alarmUri == null) {
-				alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-			}
+//			Bundle extra = null;
+//			if (extrasBundle.get("extra") != null) {
+//				extra = new Bundle();
+//				extra.putString("extra", extrasBundle.get("extra").toString());
+//			}
 
 			NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId)
+					.setDefaults(Notification.DEFAULT_ALL)
 					.setContentTitle(title)
+					.setStyle(new NotificationCompat.BigTextStyle()
+							.bigText(TEXT))
 					.setContentText(message)
 					.setSmallIcon(R.drawable.screen)
-					.setVibrate(new long[] {1000,500,1000,500,1000,500})
-					.setSound(alarmUri)
-					//.setContentIntent(launchAlarmLandingPage(context))
+					//.setVibrate(new long[] {1000,500,1000,500,1000,500})
+					//.setOngoing(true)
 					.setAutoCancel(true)
 					.setPriority(NotificationCompat.PRIORITY_HIGH)
-					.setCategory(NotificationCompat.CATEGORY_ALARM)
-					.setFullScreenIntent(startAlarmPendingIntent, true);
+					//.setTimeoutAfter(5*60*1000)
+					.setFullScreenIntent(launchAlarmLandingPage(context, extrasBundle), true)
+					.setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
 
-			NotificationCompat.Action launchAction = new NotificationCompat.Action.Builder(
-					R.drawable.launch,
-					"Launch",
-					launchAlarmLandingPage(context))
-					.build();
-
-			NotificationCompat.Action snoozeAction = new NotificationCompat.Action.Builder(
-					R.drawable.snooze,
-					"Snooze",
-					startAlarmPendingIntent)
-					.build();
-			builder.addAction(launchAction);
-			builder.addAction(snoozeAction);
-
-			if (extrasBundle.get("extra") != null) {
-				Bundle extra = new Bundle();
-				extra.putString("extra", extrasBundle.get("extra").toString());
-				builder.addExtras(extra);
-			}
-
+//			NotificationCompat.Action launchAction = new NotificationCompat.Action.Builder(
+//					R.drawable.launch,
+//					"Launch",
+//					launchAlarmLandingPage(context, extra))
+//					.build();
+//
+//			NotificationCompat.Action snoozeAction = new NotificationCompat.Action.Builder(
+//					R.drawable.snooze,
+//					"Dismiss",
+//					startAlarmPendingIntent)
+//					.build();
+			//builder.addAction(launchAction);
+			//builder.addAction(snoozeAction);
+//			if (null != extra) {
+//				builder.addExtras(extra);
+//			}
 			notificationManager.notify(146, builder.build());
 		}
 	}
 
-	private PendingIntent launchAlarmLandingPage(Context context) {
-		return PendingIntent.getActivity(context, 11, launchIntent(context), FLAG_UPDATE_CURRENT);
+	private PendingIntent launchAlarmLandingPage(Context context, Bundle extra) {
+		final Intent alarmIntent = new Intent(context, LaunchScreenActivity.class);
+
+		if (null != extra) {
+            Log.e(LOG_TAG, "launchAlarmLandingPage: " + extra.get("extra"));
+			alarmIntent.putExtras(extra);
+		}
+		alarmIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+		return PendingIntent.getActivity(context, 11, alarmIntent, FLAG_UPDATE_CURRENT);
 	}
 
 	public Intent launchIntent(Context context) {
